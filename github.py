1fb7a3186126a350ed06f2c48a9487277f80c405
kdobolyi <39930627+kdobolyi@users.noreply.github.com>
2021-01-26 15:52:48 -0500

    Update github.py

--- github.py
+++ github.py
@@ -7,6 +7,9 @@ import json
 import os
 
 import requests
+from country_list import countries_for_language
+import urllib.parse
+import re
 
 # access secret token for GitHub API to increase rate limit
 GITHUB_USERNAME = os.environ.get("GITHUB_USERNAME")
@@ -38,6 +41,42 @@ def get_contributors(repo):
 
     return committers
 
+def get_country_from_location(location_string):
+    """Return country (Hungary; United States, etc) from a text containing a city and/or state and/or country.
+    Args:
+        user: a text containing a city and/or state and/or country
+
+    Return:
+        str: a country from the list of full country names from the package country_list, or "NONE" 
+        if it wasn't a valid location or None was provided
+    """
+    # TODO: do case agnostic check
+
+    if location_string == None:
+        return "NONE"
+
+    all_countries = dict(countries_for_language('en')).values()
+    query = ""
+    pieces = location_string.split(" ")
+
+    # because the city/state could also be a country, and a country is also listed, try matching from right-to-left 
+    pieces.reverse() 
+
+    for p in pieces: 
+        p = p.strip()
+        if p in all_countries:
+            return p
+        query = urllib.parse.quote(p) + "+" + query # order matters for google search apparently
+
+    # use regex to mine the results of a google query for the country
+    regex_html = "(<span><h3 class.+><div class=.+>)([A-Za-z ]+)(</div></h3></span><span><div class=.+>Country in.+</div></span>)"
+    url = "https://www.google.com/search?q=what+%22country%22+is+" + query + "+in"
+    google_search = str(requests.get(url, allow_redirects=True).content)
+    country_search = re.search(regex_html, google_search)
+    if country_search == None:
+        return "NONE"
+    return country_search.group(2)
+
 
 def get_contributor_location(user):
     """Return geographic location, if present on github page, of user
@@ -61,4 +100,4 @@ def get_contributor_location(user):
         user_info = json.loads(response.text or response.content)
         user_location = user_info["location"]
 
-    return user_location
+    return get_country_from_location(user_location)
