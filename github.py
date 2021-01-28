2ffa091ee460e53d780ab3b825288a6b0901f9a2
John Speed Meyers <johnmeyersster@gmail.com>
2021-01-28 01:40:13 -0800

    Refactor github.py, especially get_country_from_location

--- github.py
+++ github.py
@@ -5,8 +5,9 @@ import os
 import re
 import urllib.parse
 
-from country_list import countries_for_language
 import requests
+
+from geographies_list import ALL_COUNTRIES, STATE_NAMES, STATE_ABBREV
 
 # access secret token for GitHub API to increase rate limit
 GITHUB_USERNAME = os.environ.get("GITHUB_USERNAME")
@@ -27,7 +28,8 @@ def get_contributors(repo):
     # to 500 contributors
     response = requests.get(
         "https://api.github.com/repos/" + repo + "/contributors?page=1&per_page=100",
-        auth=(GITHUB_USERNAME, GITHUB_TOKEN),
+        # convert username and token to strings per requests's specifications
+        auth=(str(GITHUB_USERNAME), str(GITHUB_TOKEN)),
     )
 
     committers = []
@@ -51,40 +53,28 @@ def get_country_from_location(location_string):
     # TODO: do case agnostic check
     # TODO: do we want to have this static list of all cities mapped to countries from 
     # https://gist.github.com/fiorix/4592774 or https://datahub.io/core/world-cities ?
+    # TODO: Kinga, do you have an intuition about the pro's and con's of having multiple
+    # return statements in one function? My spidey sense says to avoid this, but I
+    # am open to your more informed judgement and knowledge of software engineering
+    # research and practice.
 
     if location_string == None:
         return "NONE"
 
-    all_countries = dict(countries_for_language("en")).values()
-    state_names = ["Alaska", "Alabama", "Arkansas", "American Samoa", "Arizona", "California", "Colorado", 
-        "Connecticut", "District ", "of Columbia", "Delaware", "Florida", "Georgia", "Guam", "Hawaii", "Iowa", 
-        "Idaho", "Illinois", "Indiana", "Kansas", "Kentucky", "Louisiana", "Massachusetts", "Maryland", "Maine", 
-        "Michigan", "Minnesota", "Missouri", "Mississippi", "Montana", "North Carolina", "North Dakota", "Nebraska", 
-        "New Hampshire", "New Jersey", "New Mexico", "Nevada", "New York", "Ohio", "Oklahoma", "Oregon", 
-        "Pennsylvania", "Puerto Rico", "Rhode Island", "South Carolina", "South Dakota", "Tennessee", "Texas", 
-        "Utah", "Virginia", "Virgin Islands", "Vermont", "Washington", "Wisconsin", "West Virginia", "Wyoming"]
-
-    state_abbrev = ["AL", "AK", "AZ", "AR", "CA", "CO", "CT", "DC", "DE", "FL", "GA", 
-          "HI", "ID", "IL", "IN", "IA", "KS", "KY", "LA", "ME", "MD", 
-          "MA", "MI", "MN", "MS", "MO", "MT", "NE", "NV", "NH", "NJ", 
-          "NM", "NY", "NC", "ND", "OH", "OK", "OR", "PA", "RI", "SC", 
-          "SD", "TN", "TX", "UT", "VT", "VA", "WA", "WV", "WI", "WY"]
-
-
-    query = ""
     pieces = location_string.split(",")
+    end_token = pieces[-1].strip()
 
-    if "Georgia" == pieces[-1].strip():
+    if "Georgia" == end_token:
         if len(pieces) > 1:
             return "United States"
         else:
             return "Georgia"
 
-    if pieces[-1].strip() in all_countries:
-        return pieces[-1].strip()
-    if pieces[-1].strip() in state_names:
+    if end_token in ALL_COUNTRIES:
+        return end_token
+    if end_token in STATE_NAMES:
         return "United States"
-    if pieces[-1].strip() in state_abbrev:
+    if end_token in STATE_ABBREV:
         return "United States"
 
     return "NONE"
@@ -104,7 +94,9 @@ def get_contributor_location(user):
     # https://stackoverflow.com/questions/26983017/detect-ip-address-of-github-commit
     # https://gist.github.com/paulmillr/2657075
     response = requests.get(
-        "https://api.github.com/users/" + user, auth=(GITHUB_USERNAME, GITHUB_TOKEN)
+        "https://api.github.com/users/" + user,
+        # convert username and token to strings per requests's specifications
+        auth=(str(GITHUB_USERNAME), str(GITHUB_TOKEN))
     )
 
     user_location = ""
