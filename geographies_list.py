017806b6de34c230d42b149129fdd5bd4c31e1b8
John Speed Meyers <johnmeyersster@gmail.com>
2021-01-28 02:27:38 -0800

    Add dict of the world cities and countries as named constant in geography list

--- geographies_list.py
+++ geographies_list.py
@@ -1,9 +1,24 @@
 """Geography lists used to map user location text to country"""
 
+import csv
+
 from country_list import countries_for_language
 
 # a list of all countries in the english language
 ALL_COUNTRIES = dict(countries_for_language("en")).values()
+
+# static list of all cities mapped to countries
+# from https://datahub.io/core/world-cities\
+# ignore errors because of many unicode errors.
+with open("world_cities.csv", errors="ignore", newline="") as file:
+    reader = csv.reader(file)
+    city_country_list = list(reader)
+    city_country_dict = {}
+    for row in city_country_list:
+        # key is city (row[0]), value is country (row[1])
+        city_country_dict[row[0]] = row[1]
+
+CITY_COUNTRY_DICT = city_country_dict
 
 # todo: Kinga, is the separation of "District of" and "of Columbia" into
 # two separate elements a bug? Why or why not?
