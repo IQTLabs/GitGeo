d4179e87485060eb21374dff611e22fc7650197d
John Speed Meyers <johnmeyersster@gmail.com>
2021-01-29 20:05:28 -0800

    Add printing functions

---
+++ printers.py
@@ -0,0 +1,54 @@
+"""Functions for printing contributor location results"""
+
+from collections import Counter
+
+from github import get_contributor_location, get_country_from_location
+
+
+def print_by_country(contributors):
+    """Print contributors aggregated by country
+
+    Args:
+        contributors: a list of contributors
+
+    Returns:
+    null
+    """
+    print("COUNTRY | # OF CONTRIBUTORS")
+    print("---------------------------")
+    # todo: bug -- Why are there two different unique none values? Huh
+    country_list = []
+    for contributor in contributors:
+        location = get_contributor_location(contributor)
+        country = get_country_from_location(location)
+        country_list.append(country)
+
+    country_counter = Counter(country_list)
+    for country, count in country_counter.most_common():
+        print(country, count)
+
+
+def print_by_contributor(contributors, pypi_data=None):
+    """Print location results by contributor
+
+    Args:
+        contributors - a list of contributors
+
+    Returns:
+        null
+    """
+    print("CONTRIBUTOR, LOCATION")
+    if pypi_data != None:
+        print("* indicates PyPI maintainer")
+    print("---------------------")
+    for contributor in contributors:
+        location = get_contributor_location(contributor)
+        country = get_country_from_location(location)
+        try:
+            # Check if pypi_data is not None, indicating a PyPI package scan
+            if pypi_data != None and contributor in pypi_data["pypi_maintainers"]:
+                print(contributor, "*", "|", location, "|", country)
+            else:
+                print(contributor, "|", location, "|", country)
+        except UnicodeEncodeError:
+            print(contributor, "| error")
