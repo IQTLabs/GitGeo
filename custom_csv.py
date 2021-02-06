74bb05c48a0312cb4aeea932571042cba8e69e63
John Speed Meyers <johnmeyersster@gmail.com>
2021-02-06 07:01:00 -0800

    Expand csv-related functions to enable outputting csv

--- custom_csv.py
+++ custom_csv.py
@@ -1,46 +1,70 @@
 """Custom CSV-related functionality."""
 
+# pylint: disable=too-many-arguments, bad-continuation
+
 import csv
 import os
 
 
-def create_csv():
-    """Create new csv to store git-geo result
+def create_csv(results_type, software_name, timestamp):
+    """Create new csv to store GitGeo results
 
     Delete any existing csv and the create new csv.
 
     Args:
-        None
-
+        results_type - a string indicating by contributor or by country
+        software_name - name of the package or repo
+        timestamp - datetime to create unique file name
     Returns:
         None
     """
-    # delete csv if it already exists
-    filename = "git-geo-results.csv"
-    if os.path.exists(filename):
-        os.remove(filename)
+    # Create unique filename to store csv results
+    # replace slashes to avoid incorrect creation of directories
+    software_name = software_name.replace("/", "_")
+    filename = os.path.join(
+        "results", software_name + "_" + results_type + "_" + timestamp + ".csv"
+    )
 
     # Create new csv file with column names
-    with open(filename, "w") as file:
-        fieldnames = ["pkg", "username", "location"]
+    with open(filename, "w", encoding="utf-8", newline="") as file:
+        fieldnames = ["software_name", "username", "location", "country"]
         writer = csv.DictWriter(file, fieldnames=fieldnames)
         writer.writeheader()
 
 
-def add_committer_to_csv(pkg, username, location):
-    """Write committer info to existing csv file
+def add_committer_to_csv(
+    results_type, software_name, timestamp, username, location, country
+):
+    """Write committer info to existing csv file.
 
     Use to create dataset of location data for analysis.
 
     Args:
-        pkg - package name
+        results_type - a string indicating by contributor or by country
+        software_name - package name or github name
+        timestamp - datetime to append to unique existing file
         username - GitHub username
         location - Geographic info from GitHub profile
+        country - country predicted by GitGeo
 
     Returns:
         null
     """
-    with open("git-geo-results.csv", "a") as file:
-        fieldnames = ["pkg", "username", "location"]
+    # replace slashes to avoid incorrect creation of directories
+    software_name = software_name.replace("/", "_")
+    filename = os.path.join(
+        "results", software_name + "_" + results_type + "_" + timestamp + ".csv"
+    )
+    # newline='' prevents spaces in between entries. Setting encoding to utf-8
+    # ensures that most (all?) characters can be read. "a" is for append.
+    with open(filename, "a", encoding="utf-8", newline="") as file:
+        fieldnames = ["software_name", "username", "location", "country"]
         writer = csv.DictWriter(file, fieldnames=fieldnames)
-        writer.writerow({"pkg": pkg, "username": username, "location": location})
+        writer.writerow(
+            {
+                "software_name": software_name,
+                "username": username,
+                "location": location,
+                "country": country,
+            }
+        )
